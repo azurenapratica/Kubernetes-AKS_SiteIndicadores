6df0c1e2abb334b3d8063d66c54e9ccc284e8dd9
Renato Groffe <renato.groffe@yahoo.com.br>
2021-02-22 08:37:16 -0300

    Update criacao-aks.ps1

--- AKS/criacao-aks.ps1
+++ AKS/criacao-aks.ps1
@@ -3,3 +3,21 @@ az provider register -n Microsoft.ContainerService
 az aks create --resource-group TesteKubernetes --name AKSCluster --node-count 2 --generate-ssh-keys
 
 az aks get-credentials --resource-group TesteKubernetes --name AKSCluster --overwrite-existing
+
+kubectl get nodes
+
+kubectl create namespace treinamento
+
+kubectl get namespaces
+
+kubectl apply -f .\deployment.yml -n treinamento
+
+kubectl apply -f .\service.yml -n treinamento
+
+kubectl get deployments -n treinamento
+
+kubectl get deployments -n treinamento -o wide
+
+kubectl get pods -n treinamento -o wide
+
+kubectl get services -n treinamento
